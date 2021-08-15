84201faec50705c46d547966723fd0eaf64219cf
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-08-15 13:37:30 +0300

    add new post API

---
+++ main.js
@@ -0,0 +1,45 @@
+let postsArr = [];
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArr) {
+    html += `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    </hr>
+    `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((response) => response.json())
+  .then((data) => {
+    postsArr = data.slice(0, 5);
+    renderPosts();
+  });
+
+document.getElementById("new-post").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const postTitles = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body");
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      postsArr.unshift(post);
+      renderPosts();
+    });
+});
